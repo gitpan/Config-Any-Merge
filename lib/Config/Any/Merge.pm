dd7ab7f76cd79508d98b76fae9d6dbb32dba2ed3
Mario Domgörgen <mdom@cpan.org>
2008-11-13 21:26:14 +0000

    Import of MDOM/Config-Any-Merge-0.03 from CPAN.
    gitpan-cpan-distribution: Config-Any-Merge
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         MDOM/Config-Any-Merge-0.03.tar.gz
    gitpan-cpan-author:       MDOM
    gitpan-cpan-maturity:     released

--- lib/Config/Any/Merge.pm
+++ lib/Config/Any/Merge.pm
@@ -2,39 +2,9 @@ package Config::Any::Merge;
 
 use warnings;
 use strict;
+use base 'Config::Any';
 
-use Config::Any;
-
-=head1 NAME
-
-Config::Any::Merge - Merging configuration variables in a single hash instance
-
-=head1 VERSION
-
-Version 0.02
-
-=cut
-
-our $VERSION = '0.02';
-
-our @ISA = qw/ Config::Any/;
-
-=head1 SYNOPSIS
-
-Config::Any returns your configuration as a hash of hashes keyed by the
-name of the configuration file. This module merges these hashes into a
-single hash. If the C<override> paramter is set to C<0> in the paramters
-for C<load_files> or C<load_stems>, configurations files later in the
-list can't override variables that are already set. The default is to
-override previous set variables. In all other regards Config::Any::Merge
-is a strict subclass of Config::Any and inherits all of its functions.
-
-    use Config::Any::Merge;
-
-    my $cfg = Config::Any->load_files({files => \@filepaths, override => 0, ...  });
-
-=cut
-
+our $VERSION = '0.03';
 
 sub _load {
 	my($class,$args) = @_;
@@ -43,7 +13,7 @@ sub _load {
 	$args->{flatten_to_hash} = 1;
 
 	my $override = 1;
-	if (defined($args->{override})) {
+	if (defined $args->{override} ) {
 		$override = $args->{override};
 		delete $args->{override};
 	}
@@ -68,6 +38,39 @@ sub _load {
 	return \%config_merged;
 }
 
+1; # End of Config::Any::Merge
+__END__
+
+=head1 NAME
+
+Config::Any::Merge - Overrinding of configuration variables based on file order
+
+=head1 VERSION
+
+Version 0.03
+
+=cut
+
+=head1 DESCRIPTION
+
+Config::Any returns your configuration as a hash of hashes keyed by the
+name of the configuration file. This module merges these hashes into a
+single hash. If the C<override> paramter is set to C<0> in the paramters
+for C<load_files> or C<load_stems>, configurations files later in the
+list can't override variables that are already set. The default is to
+override previous set variables. In all other regards Config::Any::Merge
+is a strict subclass of Config::Any and inherits all of its functions.
+
+=head1 SYNOPSIS
+
+    use Config::Any::Merge;
+
+    my $cfg = Config::Any->load_files({files => \@filepaths, override => 0, ...  });
+
+=head1 DEPENDENCIES
+
+C<Config::Any> >= 0.15
+
 =head1 SEE ALSO
 
 C<Config::Any>
@@ -76,14 +79,9 @@ C<Config::Any>
 
 Mario Domgoergen, C<< <dom at math.uni-bonn.de> >>
 
-=head1 COPYRIGHT & LICENSE
+=head1 LICENSE AND COPYRIGTH
 
 Copyright 2008 Mario Domgoergen, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
-
-
-=cut
-
-1; # End of Config::Any::Merge
